d22dde56b54945072741c9776000fec8e2414535
Your Name <you@example.com>
2020-06-28 15:35:36 -0500

    quarkusclient2

--- QUARKUS_KAFKA/quarkusclient/src/test/java/com/okta/quarkusclient/NativeSecureResourceIT.java
+++
@@ -1,9 +0,0 @@
-package com.okta.quarkusclient;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeSecureResourceIT extends SecureResourceTest {
-
-    // Execute the same tests but in native mode.
-}
